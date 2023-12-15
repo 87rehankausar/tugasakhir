68c52cb7ff4c0dfa36ecfbedf33a861ac7a0073f
87rehankausar <147533983+87rehankausar@users.noreply.github.com>
2023-12-15 21:27:45 +0800

    Delete com.D121211087.mybook-master directory

--- com.D121211087.mybook-master/app/src/main/java/com/D121211048/mybook/AppDestinations.kt
+++
@@ -1,8 +0,0 @@
-package com.D121211048.mybook
-
-enum class AppDestinations(val title: String) {
-    MenuScreen(title = "Menu"),
-    QueryScreen(title = "Google Bookshelf"),
-    FavoriteScreen(title = "My Favorite Books"),
-    DetailScreen(title = "Book: ")
-}
--- com.D121211087.mybook-master/app/src/main/java/com/D121211048/mybook/ui/screens/detail_screen/DetailsUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.D121211048.mybook.ui.screens.detail_screen
-
-import com.D121211048.mybook.model.Book
-
-sealed interface DetailsUiState {
-    data class Success(val bookItem: Book) : DetailsUiState
-    object Error : DetailsUiState
-    object Loading : DetailsUiState
-}
