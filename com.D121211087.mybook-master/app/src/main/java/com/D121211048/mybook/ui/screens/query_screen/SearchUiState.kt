98b9abd3dbc5ebf7295e8a4853021ff04c42d37c
87rehankausar <147533983+87rehankausar@users.noreply.github.com>
2023-12-15 21:09:21 +0800

    Delete com.D121211087.mybook-master directory

--- com.D121211087.mybook-master/app/src/main/java/com/D121211048/mybook/ui/screens/query_screen/SearchUiState.kt
+++
@@ -1,6 +0,0 @@
-package com.D121211048.mybook.ui.screens.query_screen
-
-data class SearchUiState(
-    val query: String = "trump", // Notes: just because I am a lazy typer
-    val searchStarted: Boolean = false
-)
